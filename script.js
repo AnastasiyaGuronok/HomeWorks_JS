127fe709e734f59d292627b84c2eef27bc49250b
Anastasiya Guronok <nastya.guronok@gmail.com>
2024-01-28 19:27:54 +0300

    js_HW1

---
+++ script.js
@@ -0,0 +1,71 @@
+//Задание 2
+let a1=5 % 3;
+console.log(a1 +' a1:' + typeof a1);
+let a2=3 % 5;
+console.log(a2 +' a2:' + typeof a2);
+let a3=5 + '3';
+console.log(a3 +' a3:' + typeof a3);
+let a4='5' - 3;
+console.log(a4 +' a4:' + typeof a4);
+let a5=75 + 'кг';
+console.log(a5 +' a5:' + typeof a5);
+let a6=785 * 653;
+console.log(a6 +' a6:' + typeof a6);
+let a7=100 / 25;
+console.log(a7 +' a7:' + typeof a7);
+let a8=0 * 0;
+console.log(a8 +' a8:' + typeof a8);
+let a9=0 / 2;
+console.log(a9 +' a9:' + typeof a9);
+let a10=89 / 0;
+console.log(a10 +' a10:' + typeof a10);
+let a11=98 + 2;
+console.log(a11 +' a11:' + typeof a11);
+let a12=5 - 98;
+console.log(a12 +' a12:' + typeof a12);
+let a13=(8 + 56 * 4) / 5;
+console.log(a13 +' a13:' + typeof a13);
+let a14=(9 - 12) * 7 / (5+2);
+console.log(a14 +' a14:' + typeof a14);
+let a15=+"123";
+console.log(a15 +' a15:' + typeof a15);
+let a16=1 || 0;
+console.log(a16 +' a16:' + typeof a16);
+let a17=false || true;
+console.log(a17 +' a17:' + typeof a17);
+let a18=true > 0;
+console.log(a18+' a18:' + typeof a18);
+//задание 4
+let height_Cil=10;
+let VCilindra=Math.PI * ((a7 ** 2) / 4) * height_Cil;
+console.log('VCilindra=' + VCilindra);
+//задание 3
+let width = 10;
+let height = 23;
+let SPryam = width * height;
+console.log('SPryam=' + SPryam);
+//задание 5
+let radius = 5;
+let SKruga =Math.PI * (radius**2);
+console.log('SKrug=' + SKruga);
+//Задание 6
+let a=5;
+let b=7;
+let h=10
+let STrap=(a + b) * h / 2;
+console.log('STrap=' + STrap);
+//Задание 7
+let s=2000000;
+let p=10;
+let yers=5;
+let Pereplata=s * (p /100) * yers;
+console.log('Pereplata=' + Pereplata);
+//Задание 8
+let zad_8_a=8;
+let zad_8_b=3;
+let x1=(16 - zad_8_a + 2 * zad_8_b)/2;
+console.log('x1=' + x1);
+let x2=(zad_8_a - 15 * zad_8_b) / (zad_8_b -6);
+console.log('x2=' + x2);
+let x3=23780 / (1 + 2 + zad_8_a - zad_8_b);
+console.log('x3=' + x3);
