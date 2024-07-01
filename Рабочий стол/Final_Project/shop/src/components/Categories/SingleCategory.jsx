86a8339b309df8208089728be3b3808e24dded4c
Anastasiya Guronok <nastya.guronok@gmail.com>
2024-07-01 22:17:43 +0300

    feat: add online store project

---
+++ Рабочий стол/Final_Project/shop/src/components/Categories/SingleCategory.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Category from "./Category";
+import Poster from "../Poster/poster";
+
+const SingleCategory = () => {
+  return (
+    <>
+      <Poster />
+      <Category />
+    </>
+  );
+};
+
+export default SingleCategory;
